c0295cda4018b398a564b23bfbbf9d4f5d0fff5a
Samuel Torres <rillatube@gmail.com>
2019-05-06 18:46:45 -0400

    Added Components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Card.css';
+
+
+// Component Imports
+import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
+
+const CardContainer = () => {
+    return (
+        <div>
+            <a href="https://reactjs.org">
+                <CardBanner />
+                <CardContent />
+            </a>
+        </div>
+    );
+};
+
+export default CardContainer;
